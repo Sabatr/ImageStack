483259fddec39f1f7d1a4ed14b7653913abba8d7
Sabatr <bnguyen12152@gmail.com>
2018-11-22 14:21:15 +1300

    Added upload feature

--- BackEndApi/BackEndApi/Controllers/PhotosController.cs
+++ BackEndApi/BackEndApi/Controllers/PhotosController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BackEndApi.Models;
+using BackEndApi.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace BackEndApi.Controllers
 {
@@ -14,10 +19,12 @@ namespace BackEndApi.Controllers
     public class PhotosController : ControllerBase
     {
         private readonly BackEndApiContext _context;
+        private IConfiguration _configuration;
 
-        public PhotosController(BackEndApiContext context)
+        public PhotosController(BackEndApiContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/Photos
@@ -102,25 +109,25 @@ namespace BackEndApi.Controllers
         }
 
         // POST: api/Photos
-        [HttpPost("{id}")]
-        public async Task<IActionResult> PostPhoto([FromBody] Photo photo, [FromRoute] string id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //[HttpPost("{id}")]
+        //public async Task<IActionResult> PostPhoto([FromBody] Photo photo, [FromRoute] string id)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            var user = await _context.User.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            photo.UserRefId = id;
-            _context.Photo.Add(photo);
-            await _context.SaveChangesAsync();
+        //    var user = await _context.User.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    photo.UserRefId = id;
+        //    _context.Photo.Add(photo);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetPhoto", new { id = photo.PhotoId }, photo);
-        }
+        //    return CreatedAtAction("GetPhoto", new { id = photo.PhotoId }, photo);
+        //}
 
         // DELETE: api/Photos/5
         [HttpDelete("{id}")]
@@ -141,6 +148,110 @@ namespace BackEndApi.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(photo);
+        }
+
+        [HttpPost, Route("Upload/{userId}")]
+        public async Task<IActionResult> UploadFile([FromRoute] string userId,[FromForm]PhotoImageItem photoImage)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+
+            var user = await _context.User.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound("No user found");
+            }
+            try
+            {
+                using (var stream = photoImage.Image.OpenReadStream())
+                {
+                    var cloudBlock = await UploadToBlob(photoImage.Image.FileName, null, stream);
+                    //// Retrieve the filename of the file you have uploaded
+                    //var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+                    if (string.IsNullOrEmpty(cloudBlock.StorageUri.ToString()))
+                    {
+                        return BadRequest("An error has occured while uploading your file. Please try again.");
+                    }
+
+                    Photo photo = new Photo();
+                    photo.PhotoTitle = photoImage.PhotoTitle;
+                    photo.PhotoDescription = photoImage.PhotoDescription;
+                    photo.UserRefId = userId;
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
+                    photo.PhotoUrl = cloudBlock.SnapshotQualifiedUri.AbsoluteUri;
+                    photo.DateMade = DateTime.Now.ToString();
+                    _context.Photo.Add(photo);
+                    await _context.SaveChangesAsync();
+
+                    return Ok($"File: {photo.PhotoTitle} has successfully uploaded");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+
+        }
+
+        private async Task<CloudBlockBlob> UploadToBlob(string filename, byte[] imageBuffer = null, System.IO.Stream stream = null)
+        {
+
+            var accountName = _configuration["AzureBlob:name"];
+            var accountKey = _configuration["AzureBlob:key"]; ;
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference("images");
+
+            string storageConnectionString = _configuration["AzureBlob:connectionString"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Generate a new filename for every new blob
+                    var fileName = Guid.NewGuid().ToString();
+                    fileName += GetFileExtention(filename);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = imagesContainer.GetBlockBlobReference(fileName);
+
+                    if (stream != null)
+                    {
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return new CloudBlockBlob(new Uri(""));
+                    }
+
+                    return cloudBlockBlob;
+                }
+                catch (StorageException ex)
+                {
+                    return new CloudBlockBlob(new Uri(""));
+                }
+            }
+            else
+            {
+                return new CloudBlockBlob(new Uri(""));
+            }
+
+        }
+
+        private string GetFileExtention(string fileName)
+        {
+            if (!fileName.Contains("."))
+                return ""; //no extension
+            else
+            {
+                var extentionList = fileName.Split('.');
+                return "." + extentionList.Last(); //assumes last item is the extension 
+            }
         }
 
         private bool PhotoExists(int id)
---
+++ BackEndApi/BackEndApi/Models/PhotoImageItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEndApi.Models
+{
+    public class PhotoImageItem
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public string PhotoTitle { get; set; }
+        public string PhotoDescription { get; set; }
+        [Required(ErrorMessage = "Image is required")]
+        public IFormFile Image { get; set; }
+    }
+}
